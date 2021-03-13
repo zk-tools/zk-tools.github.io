77e0644b25f93576ec1242ad7832d816fc4d3471
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2021-03-13 17:22:46 +0100

    Add more fee types

--- zk-gas/zk-gas-reloader.js
+++ zk-gas/zk-gas-reloader.js
@@ -1,7 +1,9 @@
 console.log("Hello World")
 
-const new_addr    = "0x0000000000000000000000000000000000000001";
-const exist_addr  = "0x0000000000000000000000000000000000000000";
+const NEW_ADDR    = "0x0000000000000000000000000000000000000001";
+const EXIST_ADDR  = "0x0000000000000000000000000000000000000000";
+
+const WEI_TO_ETH = new bigDecimal("1000000000000000000");
 
 const CG_TOKEN = "ethereum"
 const CG_URL = "https://api.coingecko.com/api/v3/simple/price?vs_currencies=eur&ids=" + CG_TOKEN;
@@ -20,20 +22,28 @@ function do_the_thing()
 	get_eth_price(p => {
 		// console.log(`1 ETH = ${p} EUR`);
 		const price = new bigDecimal(p);
-		get_zk_price("Transfer", exist_addr, p => {
-			// console.log(`Transfer = ${p} wei ETH`);
-			const fee_eth_wei = new bigDecimal(p)
-			const fee_eth = fee_eth_wei.divide(new bigDecimal("1000000000000000000"));
-			const fee_eur = fee_eth.multiply(price);
-			const message =
-				`Transfer @ ${new Date().toISOString()} \n` +
-				`${fee_eur.getPrettyValue(3, ' ', 3)} EUR\n` +
-				`${fee_eth.getPrettyValue(3, ' ', 5)} ${ZK_TOKEN}\n` +
-				`1 ETH = ${price.getPrettyValue(3, ' ', 3)} EUR`;
-			console.log(message);
-			add_html(message.replace(/\n/g, '<br />'));
-		});
+    add_html('<hr />');
+    add_html(`1 ETH = ${price.getPrettyValue(3, ' ', 3)} EUR @ ${new Date().toISOString()}`)
+    do_the_fee_thing('Transfer     ', 'Transfer', EXIST_ADDR, price);
+    do_the_fee_thing('TransferNew  ', 'Transfer', NEW_ADDR, price);
+    do_the_fee_thing('Withdraw     ', 'Withdraw', NEW_ADDR, price);
+    do_the_fee_thing('CreateAccount', {"ChangePubKey": "ECDSA" }, EXIST_ADDR, price);
 	});
+}
+
+function do_the_fee_thing(name, type, addr, price)
+{
+  get_zk_price(type, addr, p => {
+    // console.log(`Transfer = ${p} wei ETH`);
+    const fee_eth_wei = new bigDecimal(p)
+    const fee_eth = fee_eth_wei.divide(WEI_TO_ETH);
+    const fee_eur = fee_eth.multiply(price);
+    const message =
+      `${name} = ${fee_eur.round(3).getValue()} EUR = ${fee_eth.round(6).getValue()} ${ZK_TOKEN}`;
+    console.log(message);
+    add_html(message);
+  });
+
 }
 
 function get_eth_price(cb) {
@@ -83,8 +93,8 @@ function http_post_async(url, data, cb)
 }
 
 function add_html(message) {
-   var tag = document.createElement("div");
+   var tag = document.createElement("pre");
 	 tag.innerHTML = message;
    var element = document.getElementById("new");
-   element.appendChild(tag);
+   element.prepend(tag);
 }
